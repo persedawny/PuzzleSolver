61a3240ffcad243a33ca1e6061dcc5fac39e600b
Niels Gorter <gorterniels@linuxmail.org>
2021-11-28 16:03:07 +0100

    first implementation of generator tests and generator implementation

--- PuzzleSolver-API/PuzzleSolver.API/Controllers/SudokuController.cs
+++ PuzzleSolver-API/PuzzleSolver.API/Controllers/SudokuController.cs
@@ -6,7 +6,7 @@ namespace PuzzleSolver.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class SudokuController : ControllerBase, IPuzzleController
+    public class SudokuController : ControllerBase//, //IPuzzleController
     {
         private readonly IPuzzleService service;
 
@@ -25,10 +25,21 @@ namespace PuzzleSolver.API.Controllers
         [HttpGet]
         public string Generate(int knownFields)
         {
-            // TODO: Implement after unit tests
-            return service.Generate(knownFields);
+            var puzzle = service.Generate(knownFields);
+            var isPuzzleValid = false;
+            while (!isPuzzleValid) {
+                try
+                {
+                    service.Resolve(puzzle);
+                    isPuzzleValid = true;
+
+                }
+                catch {}
+            }
+            return puzzle;
         }
 
+        [HttpGet]
         public string Resolve(string puzzleJson) => service.Resolve(puzzleJson);
     }
 }
--- PuzzleSolver-API/PuzzleSolver.Core/Generators/SudokuGenerator.cs
+++ PuzzleSolver-API/PuzzleSolver.Core/Generators/SudokuGenerator.cs
@@ -10,8 +10,46 @@ namespace PuzzleSolver.Core.Generators
 
         public override string Generate(int knownFields)
         {
-            // TODO: Implement after test
-            throw new NotImplementedException();
+            Random rnd = new Random();
+
+            var puzzleJson = "{sudoku: [" +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]," +
+                "[0, 0, 0, 0, 0, 0, 0, 0, 0]" +
+              "]}";
+
+            var fieldsFilled = 0;
+            var arr = puzzleJson.Split(',');
+
+            do
+            {
+                var row = rnd.Next(1, 10);
+                var column = rnd.Next(1, 10);
+                var idx = (row * column) - 1;
+                if (arr[idx].Contains('0'))
+                {
+                    var number = rnd.Next(1, 10);
+                    arr[idx] = arr[idx].Replace("0", $"{number}");
+                    if (base.isValid(string.Join(',', arr)))
+                    {
+                        fieldsFilled++;
+                    }
+                    else
+                    {
+                        arr[idx] = arr[idx].Replace($"{number}", "0");
+                    }
+
+                }
+            } while (fieldsFilled < knownFields);
+
+            var res = string.Join(',', arr);
+            return res;
         }
     }
 }
