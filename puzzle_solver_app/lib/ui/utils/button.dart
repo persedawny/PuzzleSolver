37986d0ae74a2d53a24a854194661d417d77f1a2
Niels Gorter <gorterniels@linuxmail.org>
2021-12-07 22:52:14 +0100

    wip added sudoku component

--- puzzle_solver_app/lib/ui/utils/button.dart
+++ puzzle_solver_app/lib/ui/utils/button.dart
@@ -5,18 +5,22 @@ class CustomButton extends StatelessWidget {
     Key? key,
     required this.label,
     required this.onPressed,
+    this.width = 200,
+    this.height = 50,
   }) : super(key: key);
 
   final Function onPressed;
   final String label;
+  final double height;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 5),
+      padding: const EdgeInsets.only(bottom: 5, right: 10, left: 10),
       child: SizedBox(
-        height: 50,
-        width: 200,
+        height: height,
+        width: width,
         child: ElevatedButton(
           onPressed: () {
             onPressed.call();
