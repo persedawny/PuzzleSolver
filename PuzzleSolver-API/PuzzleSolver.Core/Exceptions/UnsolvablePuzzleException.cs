28f2f5db7b4ce20d36ecd52a08d1fea90635ebb2
Paul de Jong <PleulSolutions@gmail.com>
2021-12-07 20:48:06 +0100

    throw custom exception on unsolvable puzzle

---
+++ PuzzleSolver-API/PuzzleSolver.Core/Exceptions/UnsolvablePuzzleException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PuzzleSolver.Core.Exceptions
+{
+    internal class UnsolvablePuzzleException : Exception
+    {
+        public UnsolvablePuzzleException()
+        {
+        }
+
+        public UnsolvablePuzzleException(string? message) : base(message)
+        {
+        }
+    }
+}
